28651e453a9ad80974d8dfcd78efcf42199a629c
Francesco Caglioti <francesco.caglioti@outlook.com>
2024-06-15 13:12:26 +0200

    Createt the Reviews Table and the Review Model

---
+++ app/Models/Review.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $table = "reviews";
+
+    protected $primaryKey = "work_id";
+    public $incrementing = false;
+    protected $keyType = "string";
+
+    public $timestamps = true;
+
+}
